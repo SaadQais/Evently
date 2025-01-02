8011bbd0bb10c13be454ac4e137badfea265172d
Saad Qais <saadqais2013@live.com>
2025-01-02 13:50:46 +0300

    add global exception handler

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/Migrations/20240409112650_Create_Database.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/Migrations/20240409112650_Create_Database.cs
@@ -12,9 +12,6 @@ namespace Evently.Modules.Events.Infrastructure.Database.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.EnsureSchema(
-                name: "events");
-
             migrationBuilder.CreateTable(
                 name: "categories",
                 schema: "events",
